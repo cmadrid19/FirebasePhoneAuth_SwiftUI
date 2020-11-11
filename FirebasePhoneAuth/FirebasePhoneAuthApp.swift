83a43c8555d9b53b2daa4d91aae95d8dc9d0dbd7
Max <maxim.macari.dam@gmail.com>
2020-11-11 09:55:26 +0100

    Initial Commit

---
+++ FirebasePhoneAuth/FirebasePhoneAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePhoneAuthApp.swift
+//  FirebasePhoneAuth
+//
+//  Created by Maxim Macari on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePhoneAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
